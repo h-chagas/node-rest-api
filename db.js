474052f6a76f0147f4184d38b055eb026e157c2d
Hugo Chagas <hugochagasuk@gmail.com>
2023-10-13 16:40:27 +0100

    database connection - follow Neon rules

---
+++ db.js
@@ -0,0 +1,16 @@
+import 'dotenv/config' // read my .env file, save all my variables to process.env to make them available here
+import postgres from 'postgres'
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+export const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
