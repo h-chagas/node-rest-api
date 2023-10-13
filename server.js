cc92f9e80300470923e36d7fb3b9b21e78957b00
Hugo Chagas <hugochagasuk@gmail.com>
2023-10-13 12:39:22 +0100

    import databaseMemory class to add videos in it + create CRUD operations

--- server.js
+++ server.js
@@ -1,6 +1,38 @@
 import { fastify } from 'fastify'
+import { DatabaseMemory } from './db-memory.js' 
 
 const server = fastify();
+
+const database = new DatabaseMemory();
+
+//Routes
+server.post('/videos', (request, reply) => {
+    database.create({
+        title: 'This is a title',
+        description: 'This is the video 1',
+        duration: '180',
+    })
+
+    console.log(database.list());
+
+    return reply.status(201).send()
+});
+
+server.get('/videos', () => {
+    return "Home page"
+});
+
+server.put('/videos/:id', () => {
+    return 'Hello'
+});
+
+server.delete('/videos/:id', () => {
+    return 'Hello'
+});
+
+
+
+
 
 server.listen({
     port: 3333
