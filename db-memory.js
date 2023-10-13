29dbb0ca4088f0ed1444fc52ebf5fab144a90d20
Hugo Chagas <hugochagasuk@gmail.com>
2023-10-13 16:53:45 +0100

    add way to operate CRUD operations in memory, before to create a relational database

--- db-memory.js
+++ db-memory.js
@@ -3,12 +3,27 @@ import { randomUUID } from 'node:crypto'
 export class DatabaseMemory {
     #videos = new Map()
 
-    list() {
-        return this.#videos.values()
+    list(search) {
+        return Array.from(this.#videos.entries())
+            .map((videoArray) => {
+                const id = videoArray[0]
+                const data = videoArray[1]
+
+                return {
+                    id,
+                    ...data,
+                }
+        })
+            .filter((video)=> {
+                if (search) {
+                    return video.title.includes(search)
+                }
+                return true
+            })
     }
 
     create(video) {
-        const videoId = randomUUID //Unique Universal ID
+        const videoId = randomUUID() //Unique Universal ID
         this.#videos.set(videoId, video)
     }
 
